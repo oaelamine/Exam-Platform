adbc042591161082945c919901d35bccde3b95f2
OURAGHI <ouraghiahmedelamine@gmail.com>
2023-03-13 07:07:46 +0100

    QCM creation function compleated

--- public/js/script.js
+++ public/js/script.js
@@ -82,88 +82,15 @@ const htmlQuestions = [
     },
 ]
 
-// Selection Questions CSS
 
-const cssQuestions = [
-    q1 = {
-        question: 'CSS stands for ______',
-        anser1: 'Color Style Sheets',
-        anser2: 'Cascade Sheets Style',
-        anser3: 'Cascade Style Sheet',
-        anserTrue: 'Cascading Style Sheets'
-    },
-    q2 = {
-        question: 'Which tag is used to embed CSS in HTML document?',
-        anser1: '<CSS>',
-        anser2: '<!DOCTYPE html>',
-        anser3: '<script>',
-        anserTrue: '<style>'
-    },
-    q3 = {
-        question: 'The <link> tag goes inside',
-        anser1: 'the title section',
-        anser2: 'the body section',
-        anser3: 'None of the above',
-        anserTrue: 'the head section'
-    },
-    q4 = {
-        question: 'Which HTML attribute is used to define inline styles ?',
-        anser1: 'styles',
-        anser2: 'Font',
-        anser3: 'class',
-        anserTrue: 'style'
-    },
-    q5 = {
-        question: 'Which property is used to increase or decrease the size of a font ?',
-        anser1: 'font-weight',
-        anser2: 'font',
-        anser3: 'None of the above',
-        anserTrue: 'font-size'
-    },
-    q6 = {
-        question: 'Which property specifies the right padding of an element ?',
-        anser1: 'padding-top',
-        anser2: 'padding-left',
-        anser3: 'padding-bottom',
-        anserTrue: 'padding-right'
-    },
-    q7 = {
-        question: 'Which property is used to capitalize text or convert text to uppercase or lowercase letters ?',
-        anser1: 'text-decoration',
-        anser2: 'text-align',
-        anser3: 'text-indent',
-        anserTrue: 'text-transform'
-    },
-    q8 = {
-        question: 'Which property is used to add or subtract space between the letters that make up a word ?',
-        anser1: 'direction',
-        anser2: 'color',
-        anser3: 'word-spacing',
-        anserTrue: 'letter-spacing'
-    },
-    q9 = {
-        question: 'Which property is used to set the background image of an element ?',
-        anser1: 'background-position',
-        anser2: 'background-repeat',
-        anser3: 'background-color',
-        anserTrue: 'background-image'
-    },
-    q10 = {
-        question: 'Which CSS framework is used to create a responsive design?',
-        anser1: 'larawell',
-        anser2: 'django',
-        anser3: 'rails',
-        anserTrue: 'bootstrap'
-    },
-]
 
 //SELECTING ELEMENTS
-let valide = document.querySelector('.submit')
 let questions = document.querySelector('.questions')
 
 
 //QCM number
 let qcmCount = htmlQuestions.length
+
 
 createQuestion(htmlQuestions, qcmCount)
 
@@ -208,14 +135,14 @@ function createQuestion(qArr, count) {
 
             //create input
             let input = document.createElement('input')
-            input.name = 'anser'
+            input.name = `anser${x+1}`
             input.type = 'radio'
-            input.id = `anser${j}`
+            input.id = `anser${x+1}_${j}`
             input.value = qArr[x][`anser${j}`]
 
             //create lable
             let label = document.createElement('label')
-            label.htmlFor = `anser${j}`
+            label.htmlFor = `anser${x+1}_${j}`
             let anser = document.createTextNode(qArr[x][`anser${j}`])
             label.appendChild(anser)
 
@@ -237,8 +164,9 @@ function createQuestion(qArr, count) {
 
         //add the ofrm to the page
         questions.appendChild(form)
-    }
 
+
+    }
     //create the button div
     let ques__btn = document.createElement('ques__btn')
     ques__btn.className = 'ques__btn'
